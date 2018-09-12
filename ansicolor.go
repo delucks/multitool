e9a8f249f8e5a5ffbbb32ef36a3d24757813094f
delucks <me@jamesluck.com>
2018-09-11 22:26:47 -0400

    Add terminal escape code module

---
+++ ansicolor.go
@@ -0,0 +1,43 @@
+package main
+
+// Used for logging messages & errors
+type ANSIColor int
+
+const (
+	Black ANSIColor = iota
+	Red
+	Green
+	Yellow
+	Blue
+	Magenta
+	Cyan
+	White
+	Reset
+)
+
+func GetEscape(c ANSIColor) string {
+	switch c {
+	case Black:
+		return "\033[0;30m"
+	case Red:
+		return "\033[0;31m"
+	case Green:
+		return "\033[0;32m"
+	case Yellow:
+		return "\033[0;33m"
+	case Blue:
+		return "\033[0;34m"
+	case Magenta:
+		return "\033[0;35m"
+	case Cyan:
+		return "\033[0;36m"
+	case White:
+		return "\033[0;37m"
+	default:
+		return "\033[0m"
+	}
+}
+
+func ColorWrap(message string, color ANSIColor) string {
+	return GetEscape(color) + message + GetEscape(Reset)
+}

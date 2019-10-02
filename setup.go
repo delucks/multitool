131e763448f74aff195bf7491f4148358019786e
delucks <ping@jamieluck.com>
2019-10-01 21:20:32 -0400

    Allow other directories for symlinking tools in place

--- setup.go
+++ setup.go
@@ -1,25 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 )
 
-func SymlinkAll(_ []string, _ io.Reader) error {
+func pathIsDir(path string) (bool, error) {
+	fh, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	return fh.IsDir(), nil
+}
+
+func SymlinkAll(args []string, _ io.Reader) error {
 	self, err := os.Executable()
 	if err != nil {
 		return err
 	}
-	bindir := filepath.Dir(self)
-	fmt.Fprintf(os.Stderr, "Symlinking tools into %s\n", bindir)
+	destinationDir := filepath.Dir(self)
+	if len(args) >= 2 {
+		// User may have passed an alternate destination directory
+		bindir := args[1]
+		ok, err := pathIsDir(bindir)
+		if err != nil {
+			return err
+		}
+		if ok {
+			destinationDir = bindir
+		} else {
+			return errors.New(bindir + " is not a valid directory")
+		}
+	}
+	fmt.Fprintf(os.Stderr, "Symlinking tools into %s\n", destinationDir)
 	for k := range Entrypoints {
 		switch k {
 		case "help", "setup":
 			continue
 		default:
-			dest := bindir + "/" + k
+			dest := destinationDir + "/" + k
 			// Check to see if the path already exists
 			if _, err = os.Stat(dest); err == nil {
 				resolved, err := filepath.EvalSymlinks(dest)
